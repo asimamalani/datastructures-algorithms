051eb1ab2a93065ed16406372f9b118d63a1819e
Asim Amalani <asim.amalani@gmail.com>
2020-01-05 19:20:31 +0000

    has pair with sum naive plus improved approach

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function containsCommonItem(a1, a2) {
   });
   //console.log(map);
   //loop through second array and check if the item exists in the HashTable.
-  for(j=0; j<a2.length; j++) {
+  for (j = 0; j < a2.length; j++) {
     const item = a2[j];
     if (map[item]) {
       return true;
@@ -47,13 +47,42 @@ function containsCommonItem(a1, a2) {
   return false;
   // if yes, return true, else false
 }
-const result = containsCommonItem(array1, array2);
-console.log(result);
+//containsCommonItem(array1, array2);
 
 //////////////////////////
 
+//[-3, 1,4,6,8,9] => 7 answer: true
+// [1,3,9,20] => 24 answer: false
+const a1 = [-3, 1, 4, 6, 8, 9];
+const a2 = [1, 3, 9, 20];
+const sum1 = 7;
+const sum2 = 24;
+
+function hasPairWithSum1(a1, s) {
+  for (let i = 0; i < a1.length; i++) {
+    for (let j = i + 1; j < a1.length; j++) {
+      if ((a1[i] + a1[j]) === s) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+//console.log(hasPairWithSum1(a1, sum1), hasPairWithSum1(a2, sum2));
 
 //////////////////////////
+function hasPairWithSum2(arr, sum) {
+  const mySet = new Set();
+  for(let i=0; i<arr.length; i++) {
+    if(mySet.has(arr[i])) {
+      return true;
+    }
+    const comp = sum-arr[i];
+    mySet.add(comp)
+  }
+  return false;
+}
+console.log(hasPairWithSum2(a1, sum1), hasPairWithSum2(a2, sum2));
 
 //////////////////////////
 
