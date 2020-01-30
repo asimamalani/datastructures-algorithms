ccdd35577b1061a8cd5c260b869fbeb259878f0d
Asim Amalani <asim.amalani@gmail.com>
2020-01-30 00:51:15 +0000

    BinarySearchTree

--- index.js
+++ index.js
@@ -625,4 +625,83 @@ function stringReverseRecursion(str) {
   if(str === "") return str;
   return stringReverseRecursion(str.substring(1)) + str.charAt(0);
 }
-stringReverseRecursion("my name is asim");
+//stringReverseRecursion("my name is asim");
+
+////////////////////////////
+
+// Binary Search Tree
+
+class BSTNode {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+//        9
+//    4      20
+//   1  6  16  170
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new BSTNode(value);
+    if(!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let currentNode = this.root;
+    while(true) {
+      if(value < currentNode.value) {
+        if(!currentNode.left) {
+          currentNode.left = newNode;
+          return this;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if(!currentNode.right) {
+          currentNode.right = newNode;
+          return this;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  }
+
+  lookup(value) {
+    if(!this.root) return null;
+    let currentNode = this.root;
+    while(currentNode) {
+      if(value === currentNode.value) {
+        return currentNode;
+      } else if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+    return null;
+  }
+}
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+JSON.stringify(traverse(tree.root));
+tree.lookup(20);
+
